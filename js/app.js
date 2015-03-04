12251937abbe80944bb30caf6457a47ca9904006
Nick Lathe <nicklathe@icloud.com>
2015-03-04 11:25:59 -0800

    Added angular scroll to brand and fixed nav position

--- js/app.js
+++ js/app.js
@@ -2,21 +2,6 @@ var myApp = angular.module('MyApp', ['ui.bootstrap', 'duScroll']);
 
 myApp.controller('MainController', ['$scope', function($scope){
 
-    // var sectionAbout = angular.element(document.getElementById('section-about'));
-    // var sectionWork = angular.element(document.getElementById('section-work'));
-    // var sectionContact = angular.element(document.getElementById('section-contact'));
-
-    // $scope.toSectionAbout = function(){
-    //       container.scrollTo(sectionAbout, 0, 2000);
-    // };
-
-    // $scope.toSectionWork = function(){
-    //     container.scrollTo(sectionWork, 0, 2000);
-    // };
-
-    // $scope.toSectionContact = function(){
-    //     container.scrollTo(sectionContact, 0, 2000);
-    // };
 
 }]);
 
---
+++ js/scroll.js
@@ -0,0 +1,7 @@
+// window.onscroll = function(){
+//     var nav = document.getElementsByClassName('navbar');
+//     nav.style.backgroundColor = 'rgba(' + 255 + ',' + 255 + ',' + 255 + ',0.9)';
+//     if(window.scrollY <= 0) {
+//         nav.style.backgroundColor = 'rgba(' + 255 + ',' + 255 + ',' + 255 + ',0.9)';
+//     }
+// };
